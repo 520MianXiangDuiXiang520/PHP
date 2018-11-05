8488c0724227eb0a27e0d01695212c9c6c12bfe5
520MianXiangDuiXiang520 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-11-05 21:00:54 +0800

    增加
    增加奇数偶数判断函数

--- c_php_yunsuan.php
+++ c_php_yunsuan.php
@@ -9,6 +9,7 @@ header("Content-type: text/html; charset=utf-8");
 $getnum1=$_POST['getnum'];
 echo "九九乘法表";
 echo '<br />';
+//九九乘法表运算，空格可以使用中文全角
 function chengfabiao()
 {
     $i1=1;
@@ -26,5 +27,28 @@ function chengfabiao()
         $j1++;
     }
 }
-chengfabiao();
+//奇数偶数判断
+function jioupanduan()
+{
+    $getnum1=$_POST['getnum'];
+    for($i=2;$i<$getnum1;$i++)
+    {
+        if($getnum1%$i==0)
+        {
+            echo '<br />';
+            echo $getnum1."是偶数！<br />";
+            echo '<br />';
+            exit();
+        }
+        else
+        {
+            echo '<br />';
+            echo $getnum1."是奇数！<br />";
+            echo '<br />';
+            exit();
+        }
+    }
+}
+chengfabiao();//乘法表
+jioupanduan();//奇数偶数
 ?>
