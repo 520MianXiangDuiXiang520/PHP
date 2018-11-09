dbaeee24d60ac8bec9ddae0a266de418cf0f193e
520MianXiangDuiXiang520 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-11-09 11:53:36 +0800

    Update c_php_shixian.php

--- c_php_shixian.php
+++ c_php_shixian.php
@@ -7,6 +7,9 @@
         h1{
             color:BLUE;
         }
+        c{
+            color: #ffdd10;
+        }
     </style>
 </head>
 
@@ -17,12 +20,52 @@
     <br />
     <br />
     <br />
-    <label for="getnum" ><c>输入数字：</c>&nbsp; &nbsp;</label>
-    <input type="text" name="getnum" />
+    <c>选择要进行的操作</c>
+    <label>
+        <select name="chose">
+            <option value="1">输出九九乘法表</option>
+            <option value="2">数组的旋转</option>
+            <option value="3">杨辉三角</option>
+            <option value="4">PI的值</option>
+            <option value="5">斐波那契数列</option>
+            <option value="6">埃拉托色尼选筛法求100以内素数</option>
+            <option value="7">1000以内的完数</option>
+        </select>
+    </label>
+    <input type="submit" value="确定"/>
     <br />
     <br />
-    <input type="submit" value="确定" />
-    <input type="reset" value="清除" />
+    <br />
+    <br />
+   <br/>
+    <c>输入要进行运算的字符串：</c>
+    <br/>
+    <input type="text" name="fanxu"/>
+    <input type="submit" value="反向输出"/>
+    <input type="reset" value="reset"/>
+
+    <br />
+    <br/>
+    <br />
+
+<h2><c>简单计算机</c></h2>
+    <input type="text" name="n1" />
+    <label>
+        <select name="fuhao">
+            <option value="+" selected="selected">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+    </label>
+    <input type="text" name="n2" />
+
+    <input type="submit" value="=" />
+   <br />
+    <br />
+    <br />
+    <input type="text" name="huiwen" />
+    <input type="submit" value="求是否是回文"/>
 </form>
 
 </body>
