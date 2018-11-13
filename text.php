dfe1ec3cadfe27145f8c9e2c4887db941f720464
520MianXiangDuiXiang520 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-11-13 22:16:51 +0800

    Update text.php
    添加注释

--- text.php
+++ text.php
@@ -1,9 +1,11 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
+//这里面用户名，密码，以及端口号必须是PHP study支持的那个phpmyadmin，初始用户名密码均为root,一直栽在着，不是自己创建的那个mysql，3306端口被占用也是这个原因
 $host = '127.0.0.1';
 $user = 'root';
 $pass = 'root';
 $port=3306;
+//注意：mysql函数已被弃用，需要使用mysqli；
 $link=@mysqli_connect("$host","$user","$pass","t2","3306");
 @mysqli_set_charset($link,"utf8");
 if(!$link)
@@ -17,6 +19,7 @@ else
 
 echo "<hr />";
 echo "<hr />";
+//新建表，table——name为：MyGuests,有三列
 $sql = "CREATE TABLE MyGuests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(30) NOT NULL,
