0215a9ca2402109a5ab2702a7715fff4cb4c1411
520MianXiangDuiXiang520 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-11-14 22:24:25 +0800

    Update new 2.php
    增加把数据加入数据库代码

--- new 2.php
+++ new 2.php
@@ -37,6 +37,49 @@ $y1=$_POST['y1'];//接收用户输入的验证码
 
   $myfile = fopen("username.txt", "a") or die("Unable to open file!");//新建文件夹用来储存username
   fwrite($myfile, $usename."#".$passname."#");//把注册的用户名和密码写入文件
+
+$host = '127.0.0.1';
+$user = 'root';
+$pass = 'root';
+$port=3306;
+$link=@mysqli_connect("$host","$user","$pass","t2","3306");
+@mysqli_set_charset($link,"utf8");
+ /*if(!$link)
+ {
+     echo '数据库连接失败:';
+ }
+ else
+ {
+     echo "链接成功";
+ }
+/*********************************************************************************************/
+/*                                                                                           */
+/*********************************创建数据表***************************************************/
+/*                                                                                            */
+/**********************************************************************************************
+$sql = "CREATE TABLE yonghu (
+username VARCHAR (25), 
+password VARCHAR(30) NOT NULL
+)";
+ if(mysqli_query($link,$sql))
+ {
+     echo "创建成功！";
+ }
+ else{
+     echo "创建数据表错误";
+ }
+****************************************************************************************************/
+
+/********************************************插入记录************************************************/
+
+/****************************************************************************************************/
+
+mysqli_select_db($link,'yonghu');
+$name=$usename;
+$passwo=$passname;
+$sq = "insert into yonghu(username,password) VALUE ('$name','$passwo')";
+ mysqli_query($link,$sq);
+
  //fopen("$YZfile","w+");//清空文件内容
  fclose($myfile);
  exit('注册成功.<a href="dengluhouduan.php">返回登陆</a>');
