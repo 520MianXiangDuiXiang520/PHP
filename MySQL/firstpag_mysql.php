0f2aac26fb0e8c255bc3a392c96c1200ca9d827c
520MianXiangDuiXiang520 <44747536+520MianXiangDuiXiang520@users.noreply.github.com>
2018-11-15 15:46:24 +0800

    首页
    首页

--- MySQL/firstpag_mysql.php
+++ MySQL/firstpag_mysql.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>首页</title>
+    <title>首 页</title>
 </head>
 <body>
 <h1>php_mysql</h1>
