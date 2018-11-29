07aa01003e0565cf80e7626dbcbb079d442fe3bb
zhangjunbaoclass <43370363+zhangjunbaoclass@users.noreply.github.com>
2018-11-29 14:09:50 +0800

    jsp实现动态菜单栏的隐藏和显现

---
+++ javascript/hind.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 2018/11/29.
+ */
+var hind=document.getElementById("yellow");
+function onck() {
+    MENU.style.display="block";//鼠标悬浮其上时显示
+}
+function show() {
+    MENU.style.display="none";//鼠标离开时隐藏
+}
---
+++ javascript/hind.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>隐藏</title>
+
+    <script src="hind.js"></script>
+    <link href="hind.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div  id="fff">
+    <ul class="nume">
+        <li onmouseover="onck()" onmouseout="show()">MENU</li>
+        <li >菜单2</li>
+        <li >菜单3</li>
+    </ul>
+
+</div>
+
+<div id="MENU"onmouseover="onck()" onmouseout="show()">
+    <ul class="">
+        <li><a href="/">Home</a></li>
+        <hr />
+        <li><a href="###">CTF资料库</a></li>
+        <hr />
+        <li><a href="###">图书管理</a></li>
+        <hr />
+        <li><a href="###">值班表</a></li>
+        <hr />
+        <li><a href="###">积分榜</a></li>
+        <hr />
+        <li><a href="###">门卡管理</a></li>
+        <hr />
+        <li><a href="###">登录</a></li>
+        <hr />
+        <li><a href="###">注册</a></li>
+    </ul>
+</div>
+</body>
+ </html>
